f64eda0b92570e0a42bf6264c249e3185e394e15
haohj <haohj94@163.com>
2020-09-02 10:28:27 +0800

    first commit

---
+++ src/test/java/com/hao/eshopcache/EshopCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.eshopcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
