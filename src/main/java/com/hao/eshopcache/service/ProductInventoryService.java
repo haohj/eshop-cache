b81835f0d7896be5b51d49e21411a786a60cd960
haohj <haohj94@163.com>
2020-11-13 15:24:51 +0800

    40_在库存服务中实现缓存与数据库双写一致性保障方案（二）

--- src/main/java/com/hao/eshopcache/service/ProductInventoryService.java
+++ src/main/java/com/hao/eshopcache/service/ProductInventoryService.java
@@ -1,34 +1,59 @@
 package com.hao.eshopcache.service;
 
+import com.hao.eshopcache.dao.RedisDAO;
+import com.hao.eshopcache.mapper.ProductInventoryMapper;
 import com.hao.eshopcache.model.ProductInventory;
+import com.hao.eshopcache.service.ProductInventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * 商品库存Service接口
+ * 商品库存Service实现类
  */
-public interface ProductInventoryService {
+@Service
+public class ProductInventoryService {
+
+    @Autowired
+    ProductInventoryMapper mapper;
+    @Autowired
+    RedisDAO redisDAO;
 
     /**
      * 根据商品id查询商品库存
+     *
      * @param productId 商品id
      * @return 商品库存
      */
-    ProductInventory findProductInventory(Integer productId);
+    public ProductInventory findProductInventory(Integer productId) {
+        return mapper.findProductInventory(productId);
+    }
 
     /**
-     * 设置商品库存的缓存
-     * @param productInventory 商品库存
+     * 设置商品库存信息到缓存中
+     *
+     * @param productInventory
      */
-    void setProductInventoryCache(ProductInventory productInventory);
+    public void setProductInventoryCache(ProductInventory productInventory) {
+        String key = "product:inventory:" + productInventory.getProductId();
+        redisDAO.set(key, String.valueOf(productInventory.getInventoryCnt()));
+    }
 
     /**
-     * 删除Redis中的商品库存的缓存
-     * @param productInventory 商品库存
+     * 移除库存缓存
+     *
+     * @param productInventory
      */
-    void removeProductInventoryCache(ProductInventory productInventory);
+    public void removeProductInventoryCache(ProductInventory productInventory) {
+        String key = "product:inventory:" + productInventory.getProductId();
+        redisDAO.delete(key);
+    }
 
     /**
-     * 更新商品库存
-     * @param productInventory 商品库存
+     * 更新商品库存信息
+     *
+     * @param productInventory
      */
-    void updateProductInventory(ProductInventory productInventory);
+    public void updateProductInventory(ProductInventory productInventory) {
+        mapper.updateProductInventory(productInventory);
+    }
 }
--- src/main/java/com/hao/eshopcache/service/UserService.java
+++ src/main/java/com/hao/eshopcache/service/UserService.java
@@ -1,23 +1,40 @@
 package com.hao.eshopcache.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.hao.eshopcache.dao.RedisDAO;
+import com.hao.eshopcache.mapper.UserMapper;
 import com.hao.eshopcache.model.User;
+import com.hao.eshopcache.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * 用户Service接口
+ * 用户Service实现类
+ * @author haohj
  */
-public interface UserService {
+@Service
+public class UserService {
 
-    /**
-     * 查询用户信息
-     *
-     * @return 用户信息
-     */
-    public User findUserInfo();
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private RedisDAO redisDAO;
 
-    /**
-     * 查询redis中缓存的用户信息
-     *
-     * @return
-     */
-    public User getCachedUserInfo();
+    public User findUserInfo() {
+        return userMapper.findUserInfo(1);
+    }
+
+    public User getCachedUserInfo() {
+        redisDAO.set("cached_user_lisi", "{\"name\": \"lisi\", \"age\":28}");
+
+        String userJson = redisDAO.get("cached_user_lisi");
+        JSONObject userJsonObject = JSON.parseObject(userJson);
+
+        User user = new User();
+        user.setName(userJsonObject.getString("name"));
+        user.setAge(userJsonObject.getInteger("age"));
+
+        return user;
+    }
 }
